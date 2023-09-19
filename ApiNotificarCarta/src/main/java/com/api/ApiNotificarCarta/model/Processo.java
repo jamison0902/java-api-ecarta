f4428cf423499c008f13cb2007ad3d3fe3da9b54
Jamison Queiroz <111821155+jamison0902@users.noreply.github.com>
2023-09-19 19:01:45 -0300

    Add files via upload
    Serializable

---
+++ ApiNotificarCarta/src/main/java/com/api/ApiNotificarCarta/model/Processo.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.api.ApiNotificarCarta.model;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@Entity
+public class Processo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String documento;
+
+    private String email;
+
+    private String cep;
+
+    @Column(unique = true, nullable = false)
+    private String numeroProcesso;
+
+    private boolean notificado;
+
+    private String logradouro;
+
+    private String complemento;
+
+    private String bairro;
+
+    private String localidade;
+
+    private String uf;
+
+}
